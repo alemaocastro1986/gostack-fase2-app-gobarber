a67044c77533a3fe09bc3b9bc38234d4203ce71f
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2020-06-18 15:26:56 -0300

    Add service to controller

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
-    //
+    const showProfile = container.resolve(ShowProfileService);
+
+    const user = await showProfile.execute({ user_id: request.user.id });
+    return response.json(user);
   }
 
   public async updated(
